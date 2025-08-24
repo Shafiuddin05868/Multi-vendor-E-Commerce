cb537a0e6e5e33064d53a1dec5c60ea111802278
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-08-24 23:47:25 +0600

    db connect

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/authRoutes";
+import dbConnect from "./utiles/db";
 const app = express();
 dotenv.config();
 app.use(express.json());
@@ -9,8 +10,11 @@ app.use("/api", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
+})
 
-app.listen(process.env.port, () => {
-  console.log(`Server is running on port ${process.env.port}`);
-});
+;(async () => {
+  await dbConnect();
+  app.listen(process.env.port, () => {
+    console.log(`Server is running on port ${process.env.port}`);
+  });
+})();
---
+++ backend/src/utiles/db.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+const dbConnect = async()=>{
+    try {
+        const databaseUrl = process.env.databaseUrl
+        if (!databaseUrl) {
+            throw new Error('Database URL is not defined in environment variables')
+        }
+        await mongoose.connect(databaseUrl)
+        console.log('Database connected')
+    } catch (err) {
+        console.error((err as Error).message)
+    }
+}
+
+export default dbConnect;
