6ee43b8c2b68331851feb3f0d1011a8215ce4f35
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-09-06 00:43:33 +0600

    feat: setup main layout with sidebar

--- dashboard/app/layout/MainLayout.tsx
+++ dashboard/app/layout/MainLayout.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { Outlet } from 'react-router'
-import { AdminHeader as Header } from './Header'
-import Sidebar from './Sidebar'
+import React from "react";
+import { Outlet } from "react-router";
+import { AdminHeader as Header } from "./Header";
+import Sidebar from "./Sidebar";
 
 const MainLayout = () => {
   return (
-    <div>
-      <Header />
+    <div className="flex h-screen overflow-hidden">
       <Sidebar />
-      <div>
-        <Outlet/>
+      <div className="flex-1 overflow-auto">
+        <Header />
+        <main className="p-6">
+          <Outlet />
+        </main>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
